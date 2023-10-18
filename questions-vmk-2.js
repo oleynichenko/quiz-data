71b8bd871ccf22970b917a2c5ca044ee8d728c70
Alex <oleynichenkos@gmail.com>
2023-10-18 22:04:11 +0300

    add vmk-2 questions

---
+++ questions-vmk-2.js
@@ -0,0 +1,197 @@
+const questionsData = [
+  {
+    wording: 'Сколько в итоге есть ступеней подъема к Творцу по мнению Бааль Сулама? ',
+    pointsAvailable: 0.8,
+    options: {
+      a: '5',
+      b: '25',
+      c: '125',
+      d: '613',
+    },
+    correctOptions: ['c'],
+    themes: ['vmk-2']
+  },
+  {
+    wording: 'Согласно книге "Древо Жизни", причина создания всех миров в том, что Творец обязан во всех своих действиях быть ...',
+    pointsAvailable: 0.8,
+    options: {
+      a: 'добрым',
+      b: 'совершенным',
+      c: 'бесконечным',
+      d: 'Творец никому ничего не должен.',
+    },
+    correctOptions: ['b'],
+    themes: ['vmk-2']
+  },
+  {
+    wording: 'Основой творения является только ...',
+    pointsAvailable: 0.8,
+    options: {
+      a: 'желание получать',
+      b: 'желание отдавать',
+    },
+    correctOptions: ['a'],
+    themes: ['vmk-2']
+  },
+  {
+    wording: 'Как переводится с иврита слово "сгула"?',
+    pointsAvailable: 1,
+    options: {
+      a: 'отраженный свет',
+      b: 'свет милосердия',
+      c: 'свет мудрости',
+      d: 'Нет правильных ответов',
+    },
+    correctOptions: ['d'],
+    themes: ['vmk-2']
+  },
+  {
+    wording: 'Слияние с Творцом и получение света это одно и то же.',
+    pointsAvailable: 1,
+    options: {
+      a: 'Да',
+      b: 'Нет',
+    },
+    correctOptions: ['b'],
+    themes: ['vmk-2']
+  },
+  {
+    wording: 'Выберите названия используемые в 4-ех стадиях прямого света.',
+    pointsAvailable: 1,
+    options: {
+      a: 'Хохма',
+      b: 'Бина',
+      c: 'Хесед',
+      d: 'Малхут',
+    },
+    correctOptions: ['a', 'b', 'd'],
+    themes: ['vmk-2']
+  },
+  {
+    wording: 'Как называется первая стадия прямого света?',
+    pointsAvailable: 0.8,
+    options: {
+      a: 'Кетер',
+      b: 'Хохма',
+      c: 'Бина',
+      d: 'Даат',
+    },
+    correctOptions: ['b'],
+    themes: ['vmk-2']
+  },
+  {
+    wording: 'Какой букве тетраграмматона соответствует Бина, как стадия прямого света?',
+    pointsAvailable: 0.8,
+    options: {
+      a: 'Йуд',
+      b: 'Хей',
+      c: 'Вав',
+    },
+    correctOptions: ['b'],
+    themes: ['vmk-2']
+  },
+  {
+    wording: 'Какой свет содержит первая стадия прямого света?',
+    pointsAvailable: 1,
+    options: {
+      a: 'Ор Хохма',
+      b: 'Ор Хасадим',
+      c: 'Свет мудрости',
+      d: 'Свет милосердия',
+    },
+    correctOptions: ['a', 'c'],
+    themes: ['vmk-2']
+  },
+  {
+    wording: 'Вторая стадия прямого света это...',
+    pointsAvailable: 1,
+    options: {
+      a: 'Кетер',
+      b: 'Хохма',
+      c: 'Тиферет',
+      d: 'Нет правильных ответов',
+    },
+    correctOptions: ['d'],
+    themes: ['vmk-2']
+  },
+  {
+    wording: 'В какой стадии присутвует Ор Хасадим?',
+    pointsAvailable: 1,
+    options: {
+      a: 'Первой',
+      b: 'Второй',
+      c: 'Третьей',
+      d: 'Нет правильных ответов',
+    },
+    correctOptions: ['a', 'c'],
+    themes: ['vmk-2']
+  },
+  {
+    wording: 'Как называется последняя стадия из 4-ех стадий прямого света?',
+    pointsAvailable: 1,
+    options: {
+      a: 'Хесед',
+      b: 'Гвура',
+      c: 'Тиферет',
+      d: 'Нет правильных ответов',
+    },
+    correctOptions: ['d'],
+    themes: ['vmk-2']
+  },
+  {
+    wording: 'Какой мир соответствует 3-ей стадии прямого света?',
+    pointsAvailable: 1,
+    options: {
+      a: 'Ацилут',
+      b: 'Брия',
+      c: 'Ецира',
+      d: 'Асия',
+    },
+    correctOptions: ['c'],
+    themes: ['vmk-2']
+  },
+  {
+    wording: 'Что из перечисленного не относится к названиям стадий прямого света?',
+    pointsAvailable: 1,
+    options: {
+      a: 'Кетер',
+      b: 'Хохма',
+      c: 'Бина',
+      d: 'Даат',
+      e: 'Хесед',
+      f: 'Гвура',
+    },
+    correctOptions: ['a', 'd', 'e', 'f'],
+    themes: ['vmk-2']
+  },
+  {
+    wording: 'Какой свет наполняет третью стадию прямого света?',
+    pointsAvailable: 1.2,
+    options: {
+      a: 'Ор Хохма',
+      b: 'Ор Хасадим',
+    },
+    correctOptions: ['a', 'b'],
+    themes: ['vmk-2']
+  },
+  {
+    wording: 'Что является сосудом для получения Ор Хасадим?',
+    pointsAvailable: 1,
+    options: {
+      a: 'Желание получать',
+      b: 'Желание отдавать',
+    },
+    correctOptions: ['b'],
+    themes: ['vmk-2']
+  },
+  {
+    wording: 'Что является сосудом в первой стадии прямого света?',
+    pointsAvailable: 1,
+    options: {
+      a: 'Желание получать',
+      b: 'Желание отдавать',
+    },
+    correctOptions: ['a'],
+    themes: ['vmk-2']
+  },
+];
