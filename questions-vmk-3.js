3fca9a6940e4fdab94b77aa12bd12bdf97a994ba
Alex <oleynichenkos@gmail.com>
2023-10-24 19:01:06 +0300

    add vmk-3 questions

---
+++ questions-vmk-3.js
@@ -0,0 +1,44 @@
+const questionsData = [
+  {
+    wording: 'У Творца нет желания получать.',
+    pointsAvailable: 0.8,
+    options: {
+      a: 'Верно',
+      b: 'Не верно',
+    },
+    correctOptions: ['a'],
+    themes: ['vmk-3']
+  },
+  {
+    wording: 'Некоторые духовные объекты могут существовать без масаха.',
+    pointsAvailable: 0.8,
+    options: {
+      a: 'Верно',
+      b: 'Не верно',
+    },
+    correctOptions: ['b'],
+    themes: ['vmk-3']
+  },
+  {
+    wording: 'Можно ли в духовном получить свет без масаха?',
+    pointsAvailable: 0.8,
+    options: {
+      a: 'Да',
+      b: 'Нет',
+    },
+    correctOptions: ['b'],
+    themes: ['vmk-3']
+  },
+  {
+    wording: 'Что такое "нер дакик" согласно словарю курса?',
+    pointsAvailable: 1,
+    options: {
+      a: 'Ударное взаимодействие света и сосуда',
+      b: 'Отраженный свет',
+      c: 'Обратная сторона святости',
+      d: 'Нет правильных ответов.',
+    },
+    correctOptions: ['d'],
+    themes: ['vmk-3']
+  },
+];
