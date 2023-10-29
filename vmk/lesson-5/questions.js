6c738fffc7618190e449585435227c61549e4e15
Alexandr Oleynichenko <oleynichenkos@gmail.com>
2023-10-29 11:22:35 +0200

    add vmk 1 and 5 questions

---
+++ vmk/lesson-5/questions.js
@@ -0,0 +1,168 @@
+const questionsData = [
+  {
+    wording: 'Связи между сфирот более легки для понимания, чем связи между парцуфим.',
+    pointsAvailable: 0.8,
+    options: {
+      a: 'Да',
+      b: 'Нет',
+    },
+    correctOptions: ['a'],
+    themes: ['vmk-5']
+  },
+  {
+    wording: 'Что получила четвертая стадия как средство исправления после цимцума?',
+    pointsAvailable: 1,
+    options: {
+      a: 'Решимо',
+      b: 'Масах',
+      c: 'Экран',
+      d: 'Авиют',
+    },
+    correctOptions: ['b', 'c'],
+    themes: ['vmk-5']
+  },
+  {
+    wording: 'В каком мире появляется первый парцуф?',
+    pointsAvailable: 1,
+    options: {
+      a: 'Ацилут',
+      b: 'Брия',
+      c: 'Ецира',
+      d: 'Нет правильных ответов.',
+    },
+    correctOptions: ['d'],
+    themes: ['vmk-5']
+  },
+  {
+    wording: 'Как называется первый парцуф в мире Адам Кадмон?',
+    pointsAvailable: 1,
+    options: {
+      a: 'Гальгольта',
+      b: 'А”Б',
+      c: 'СА”Г',
+      d: 'Нет правильных ответов.',
+    },
+    correctOptions: ['d'],
+    themes: ['vmk-5']
+  },
+  {
+    wording: 'Сколько основных парцуфим есть в мире Адам Кадмон?',
+    pointsAvailable: 0.8,
+    options: {
+      a: '4',
+      b: '5',
+      c: '10',
+      d: 'Нет правильных ответов.',
+    },
+    correctOptions: ['b'],
+    themes: ['vmk-5']
+  },
+  {
+    wording: 'Что становится зачатками келим в результате зивуг-дэ-акаа?',
+    pointsAvailable: 0.8,
+    options: {
+      a: 'Ор Хозер',
+      b: 'Ор Яшар',
+      c: 'Ор Пними',
+      d: 'Нет правильных ответов.',
+    },
+    correctOptions: ['a'],
+    themes: ['vmk-5']
+  },
+  {
+    wording: 'Сколько светов перечисляется в абрревиатуре НаРаНХ?',
+    pointsAvailable: 1,
+    options: {
+      a: '1',
+      b: '4',
+      c: '5',
+      d: 'Нет правильных ответов.',
+    },
+    correctOptions: ['b'],
+    themes: ['vmk-5']
+  },
+  {
+    wording: "В абрревиатуре НаРаН перечисляется... ",
+    pointsAvailable: 0.8,
+    options: {
+      a: '3 света',
+      b: '4 света',
+      c: '5 светов',
+      d: 'Нет правильных ответов.',
+    },
+    correctOptions: ['a'],
+    themes: ['vmk-5']
+  },
+  {
+    wording: "Выберите из перечисленного названия светов.",
+    pointsAvailable: 0.8,
+    options: {
+      a: 'Нахаш',
+      b: 'Брия',
+      c: 'Ехида',
+      d: 'Гальгальта',
+    },
+    correctOptions: ['c'],
+    themes: ['vmk-5']
+  },
+  {
+    wording: "Что из перечисленного не является названиями светов, входящих в аббревиатуру НаРаНХа”Й?",
+    pointsAvailable: 1,
+    options: {
+      a: 'Нефеш',
+      b: 'Руах',
+      c: 'Хохма',
+      d: 'Йесод',
+      e: 'Авиют',
+    },
+    correctOptions: ['c', 'd', 'e'],
+    themes: ['vmk-5']
+  },
+  {
+    wording: "В каком виде скрытия у человека не присутствует Творец в картине мира?",
+    pointsAvailable: 0.8,
+    options: {
+      a: 'В простом скрытии',
+      b: 'В двойном скрытии',
+      c: 'Нет правильных ответов',
+    },
+    correctOptions: ['b'],
+    themes: ['vmk-5']
+  },
+  {
+    wording: 'Что такое "решимо"?',
+    pointsAvailable: 0.8,
+    options: {
+      a: 'Запись',
+      b: 'Душа',
+      c: 'Сила желания',
+    },
+    correctOptions: ['a'],
+    themes: ['vmk-5']
+  },
+  {
+    wording: 'Что такое "авиют" согласно словарю курса?',
+    pointsAvailable: 1,
+    options: {
+      a: 'Сила желания',
+      b: 'Душа',
+      c: 'Свет',
+      d: 'Нет правильных ответов',
+    },
+    correctOptions: ['d'],
+    themes: ['vmk-5']
+  },
+  {
+    wording: 'Какие света отсутствуют в парцуфе СА”Г?',
+    pointsAvailable: 1.2,
+    options: {
+      a: 'Нефеш',
+      b: 'Руах',
+      c: 'Нешама',
+      d: 'Хая',
+      e: 'Йехида',
+    },
+    correctOptions: ['d', 'e'],
+    themes: ['vmk-5']
+  },
+];
