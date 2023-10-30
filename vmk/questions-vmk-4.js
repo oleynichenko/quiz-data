99adbe7541c2bb5de248ab5a72e4a87a1ab66788
Alex <oleynichenkos@gmail.com>
2023-10-30 21:12:19 +0200

    add vmk questions

---
+++ vmk/questions-vmk-4.js
@@ -0,0 +1,37 @@
+const questionsData = [
+  {
+    wording: 'Ор Хозер + Ор Пними = ?',
+    pointsAvailable: 1.2,
+    options: {
+      a: 'Ор Макиф',
+      b: 'Ор Яшар',
+      c: 'Нет правильных ответов.',
+    },
+    correctOptions: ['b'],
+    themes: ['vmk-4']
+  },
+  {
+    wording: 'Как называется новый сосуд, появившийся вместо четвертой стадии?',
+    pointsAvailable: 1,
+    options: {
+      a: 'Прямой свет',
+      b: 'Внутренний свет',
+      c: 'Масах',
+      d: 'Нет правильных ответов.',
+    },
+    correctOptions: ['d'],
+    themes: ['vmk-4']
+  },
+  {
+    wording: 'Как переводится на иврит "внутренний свет"?',
+    pointsAvailable: 0.8,
+    options: {
+      a: 'Ор Яшар',
+      b: 'Ор Макиф',
+      c: 'Ор Пними',
+      d: 'Нет правильных ответов.',
+    },
+    correctOptions: ['c'],
+    themes: ['vmk-4']
+  },
+];
